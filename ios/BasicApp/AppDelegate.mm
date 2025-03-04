059eef599f2dbedaf762bbfe3dc125dc7bbcd3b0
Aarya C <abchhatkuli@gmail.com>
2025-03-03 20:45:02 -0500

    fix appdelegate dependency

--- ios/BasicApp/AppDelegate.mm
+++ ios/BasicApp/AppDelegate.mm
@@ -1,14 +1,14 @@
-#import "AppDelegate.h"
+#import "RCTAppDelegate.h"
+#import <React/RCTBundleURLProvider.h> // Ensure this is imported
 
-#import <React/RCTBundleURLProvider.h>
+@interface AppDelegate : RCTAppDelegate
+@end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"BasicApp";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
